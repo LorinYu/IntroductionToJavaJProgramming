2e558a9f1e9861e0129f9304f7f7c8d5a7dea780
lorin <1181401581@qq.com>
2017-08-08 21:50:40 +0800

    commit 170808

--- src/com/sdutiot/grammer/ChapterEight.java
+++ src/com/sdutiot/grammer/ChapterEight.java
@@ -1,14 +1,16 @@
 package com.sdutiot.grammer;
 
+import java.io.IOException; 
+
 public class ChapterEight {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    function4();
+    function5();
   }
   
   public static void function1() {
-    String message = new String("welcome to java!");
+    //String message = new String("welcome to java!");
     String s = "this is a dog.";
     s = "html";
     System.out.println(s.length());  
@@ -24,6 +26,7 @@ public class ChapterEight {
     message = message.trim();
     System.out.println(message);
   }
+  
   public static void function4() {
     StringBuffer str = new StringBuffer();
     str = str.append("C");
@@ -31,5 +34,17 @@ public class ChapterEight {
     str = str.append("!");
     str.insert(1, "ent");
     System.out.println(str);
+  }
+  public static void function5() {
+    java.io.File file = new java.io.File("txt1.txt");
+    try {  
+      file.createNewFile(); // 创建文件  
+    } catch (IOException e) {  
+      e.printStackTrace();  
+    }
+    //System.out.println("does it exist? " + file.exists());
+    //System.out.println("Can it be read? " + file.canRead());
+    //System.out.println(new java.util.Date(file.lastModified()));
+    //java.io.printWriter output = new java.io.PrintWriter(file);
   }
 }
