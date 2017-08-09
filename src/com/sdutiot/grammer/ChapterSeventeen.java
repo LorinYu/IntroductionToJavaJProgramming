ec118b48aeea4e88ade86051897603d7f8a2beaa
lorin <1181401581@qq.com>
2017-08-09 21:56:07 +0800

    commit 170809

--- src/com/sdutiot/grammer/ChapterSeventeen.java
+++ src/com/sdutiot/grammer/ChapterSeventeen.java
@@ -1,10 +1,32 @@
 package com.sdutiot.grammer;
 
+import java.util.*;
+
 public class ChapterSeventeen {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    function1();
   }
-
+  
+  public static void function1() {
+    Scanner scanner = new Scanner(System.in);
+    boolean continueInput = true;
+    
+    do {
+      try {
+        System.out.println("Enter an integer: ");
+        int number = scanner.nextInt();
+      
+        //display the result 
+        System.out.println("The number entered is " + number);
+        continueInput = false;
+        //scanner.close();
+      }
+      catch (InputMismatchException ex) {
+        System.out.println("incorrect input; an integer is rquired.");
+        scanner.nextLine();
+      }
+    } while (continueInput);
+    scanner.close();
+  }
 }
